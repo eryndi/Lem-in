b8e251a6e82abb9b71eb8bdc73e50079e19b39e9
Daniel Hadley <hadleydaniel@gmail.com>
2017-12-04 17:32:28 +0100

    add test maps and finish parsing rooms up to tubes

--- src/parse.c
+++ src/parse.c
@@ -6,9 +6,11 @@
 /*   By: dhadley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 16:31:26 by dhadley           #+#    #+#             */
-/*   Updated: 2017/12/02 18:12:20 by dhadley          ###   ########.fr       */
+/*   Updated: 2017/12/04 16:39:13 by dhadley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "lemin.h"
 
 /*
 ** Check if char * is bigger than INTMAX
@@ -17,27 +19,42 @@
 static int	parse_ants(t_lemin *data)
 {
 	char	*line;
+	char	*tmp;
 
 	if (get_next_line(0, &line) != 1)
+	{
+		ft_putstr("ERROR reading line\n");
 		return (0);
+	}
 	if (!*line)
+	{
+		ft_putstr("ERROR empty line\n");
 		return (0);
+	}
 	tmp = line;
 	while (*line)
 	{
 		if (!ft_isdigit(*line))
+		{
+			ft_putstr("ERROR number of ants not an int\n");
 			return (0);
+		}
 		line++;
 	}
 	data->num_ants = ft_atoi(tmp);
 	if (data->num_ants < 1)
+	{
+		ft_putstr("ERROR wrong number of ants\n");
 		return (0);
-	ft_list_push_end(data->lines, tmp);
+	}
+	ft_list_push_end(&data->lines, tmp);
+	ft_dprintf(1, "finished parsing ants, there are %d of them\n", data->num_ants); ////
 	return (1);
 }
 
 int		parse(t_lemin *data)
 {
-	if (!parse_ants(data) || parse_rooms(data))
+	if (!parse_ants(data) || !parse_rooms(data))
 		return (0);
+	return (1);
 }
