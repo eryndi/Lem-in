90c0a74407098fdf4a708fe52c6f19fcaa2c73b9
Diana WALD <dwald@e1r1p14.42.fr>
2017-12-15 10:05:41 +0100

    enabled flags for compilation in Makefile and corrected main accordingly

--- src/main.c
+++ src/main.c
@@ -6,47 +6,11 @@
 /*   By: dwald <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 14:19:25 by dwald             #+#    #+#             */
-/*   Updated: 2017/12/14 16:28:47 by dwald            ###   ########.fr       */
+/*   Updated: 2017/12/15 10:04:15 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lemin.h>
-/*
-void	test_map(t_lemin *data)
-{
-	t_room *tmp1;
-	t_room *tmp2;
-
-
-	tmp1 = data->rooms;
-	while (!tmp1->is_start)
-	{
-		tmp1 = tmp1->parse_next;
-	}
-	tmp2 = tmp1->next_start[1];
-	tmp1 = tmp1->next_start[0];
-	ft_putstr("\n The first path is: ");
-	while (tmp1)
-	{
-		ft_dprintf(1, "%s - ", tmp1->name);
-		tmp1 = tmp1->next;
-	}
-	ft_putstr("\n The second path is: ");
-	while (tmp2)
-	{
-		ft_dprintf(1, "%s - ", tmp2->name);
-		tmp2 = tmp2->next;
-	}
-	ft_putchar('\n');
-	return;
-
-}
-*/
-static	int	ft_error(void)
-{
-	ft_putendl(strerror(errno));
-	exit(1);
-}
 
 int				main(void)
 {
