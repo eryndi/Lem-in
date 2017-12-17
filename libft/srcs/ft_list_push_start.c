0a86c2558f001a83ccf3e28fdf45fe3221593d32
Diana WALD <dwald@e1r1p11.42.fr>
2017-12-17 16:14:54 +0100

    norme for ft_list_push_start.c

--- libft/srcs/ft_list_push_start.c
+++ libft/srcs/ft_list_push_start.c
@@ -6,7 +6,7 @@
 /*   By: dhadley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 19:23:17 by dhadley           #+#    #+#             */
-/*   Updated: 2017/12/01 19:28:01 by dhadley          ###   ########.fr       */
+/*   Updated: 2017/12/17 16:12:54 by dwald            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_list_push_start(t_list **begin_list, void *content)
 {
 	t_list *new_head;
-	
+
 	new_head = ft_list_new_elem(content);
 	new_head->next = *begin_list;
 	*begin_list = new_head;
